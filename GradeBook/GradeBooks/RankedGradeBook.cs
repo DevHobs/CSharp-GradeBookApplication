fb094d75d3e0fe533be97d80bfd97683e1fd93fb
DevHobs <devhobs@gmail.com>
2022-10-06 21:18:23 -0400

    Step3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,5 +11,18 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count() > 25) { Console.WriteLine("A")} else;
+            if (Students.Count() > 20) { } else Console.WriteLine("B");
+            if (Students.Count() > 15) { } else Console.WriteLine("C");
+            if (Students.Count() > 10) { } else Console.WriteLine("D");
+            if (Students.Count() > 5) { } else Console.WriteLine("F");
+            {
+                InvalidOperationException invalidOperationException = new InvalidOperationException;
+                throw invalidOperationException;
+            }
+        }
     }
 }
+
