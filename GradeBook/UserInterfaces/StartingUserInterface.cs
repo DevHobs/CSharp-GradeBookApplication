c4645f18f3896ba6532eec89273aea735f95b5ec
DevHobs <devhobs@gmail.com>
2022-10-17 16:46:20 -0400

    Step5

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,12 +34,11 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
-            {
-                Console.WriteLine("Command not valid, Create requires a name.");
-                return;
-            }
+            if (parts.Length != 3)
+            {Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
+                return;}
             var name = parts[1];
+            var gradeBook = parts[2];
             BaseGradeBook gradeBook = new BaseGradeBook(name);
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
@@ -49,10 +48,8 @@ namespace GradeBook.UserInterfaces
         {
             var parts = command.Split(' ');
             if (parts.Length != 2)
-            {
-                Console.WriteLine("Command not valid, Load requires a name.");
-                return;
-            }
+            {Console.WriteLine("Command not valid, Load requires a name.");
+                return;}
             var name = parts[1];
             var gradeBook = BaseGradeBook.Load(name);
 
