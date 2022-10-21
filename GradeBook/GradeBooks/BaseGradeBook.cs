6d1cad7e988502a763c2a8115ca50c76f558663f
DevHobs <devhobs@gmail.com>
2022-10-20 21:49:38 -0400

    16

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,9 +14,9 @@ namespace GradeBook.GradeBooks
         public GradeBookType Type { get; set; }
         public string Name { get; set; }
         public List<Student> Students { get; set; }
-        public bool IsWeighted { get; set; }
+        public bool isWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
             Name = name;
             Students = new List<Student>();
@@ -108,20 +108,28 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var gpa = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    gpa = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    gpa = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    gpa = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    gpa = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    gpa = 0;
+                    break;
             }
-            return 0;
+            if (isWeighted && (studentType == studentType.Honors || studentType.DualEnrolled))
+                gpa++;
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
